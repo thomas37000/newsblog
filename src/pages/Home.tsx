5212af18e734f24a6fc97fbb367b8aeecc7f75d0
thomas37000 <thomas.chalanson@sfr.fr>
2023-03-02 17:20:22 +0100

    style the Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -69,37 +69,20 @@ const Home: React.FunctionComponent = () => {
     loadCoins();
   }, []);
 
-  const randomCoins = coins.sort(() => Math.random() - 0.5).slice(0, 3);
-
   return (
     <>
       {/* COINS */}
-      <div>
-        <HomeCoinList coins={coins} />
-      </div>
+      <section className='py-6'>
+        <div className='flex justify-center'>
+          <HomeCoinList coins={coins} />
+        </div>
+      </section>
 
       {/* CINEMA */}
-      <div
-        className='flex justify-center p-4 my-8 text-sm text-gray-700 bg-gray-100 rounded-lg dark:bg-gray-800 dark:text-gray-300'
-        role='alert'
-      >
-        <svg
-          aria-hidden='true'
-          className='flex-shrink-0 inline w-5 h-5 mr-3'
-          fill='currentColor'
-          viewBox='0 0 20 20'
-          xmlns='http://www.w3.org/2000/svg'
-        >
-          <path
-            fill-rule='evenodd'
-            d='M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z'
-            clip-rule='evenodd'
-          ></path>
-        </svg>
-        <span className='sr-only'>Info</span>
-        <div>
-          <span className='font-medium'></span> Envie d'aller voir un film voici
-          les 3 meilleurs films récents les mieux notés par les internautes.{' '}
+      <section className='py-6'>
+        <div className='text-xl font-bold leading-none text-gray-900 dark:text-white'>
+          Envie d'aller voir un film voici les 3 meilleurs films récents les
+          mieux notés par les internautes.
           <p>
             Sinon allez voir les autres films sur la page{' '}
             <Link to='/movies' className='text-red-600'>
@@ -107,9 +90,9 @@ const Home: React.FunctionComponent = () => {
             </Link>
           </p>
         </div>
-      </div>
 
-      <div className='flex items-stretch'>{fetchMovies}</div>
+        <div className='flex justify-center mt-8 space-x-4'>{fetchMovies}</div>
+      </section>
     </>
   );
 };
